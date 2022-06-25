b94b85c1ee98d7a15eb2c2fe0173b3031121b0c1
BenBos71 <ben.bos1097@gmail.com>
2022-06-24 20:57:55 -0400

    Base Gameplay From Tutorial

---
+++ HitTheTree/HitTheTree/Helper/VectorOperations.swift
@@ -0,0 +1,18 @@
+//
+//  VectorOperations.swift
+//  HitTheTree
+//
+//  Created by Ben Bos on 6/22/22.
+//
+
+import Foundation
+import SceneKit
+
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3(x:left.x + right.x, y: left.y + right.y, z: left.z + right.z)
+}
+
+
+func +=( left: inout SCNVector3, right: SCNVector3) {
+    left = left + right
+}
